107376b2fc14998c7949f5a69f06abfbe016e0f7
fail2ban1337 <abelomar@e2r7p12.1337.ma>
2021-06-12 17:34:01 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/wellpaperapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wellpaperapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/home.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:wellpaperapp/data/data.dart';
+import 'package:wellpaperapp/model/catergories_model.dart';
+import 'package:wellpaperapp/views/widgets/widget.dart';
+import 'package:http/http.dart' as http;
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<CategorieModel> categories = [];
+
+  getTrendingWallpaper() async {
+    var response = await http.get(
+        Uri.parse(
+          "https://api.pexels.com/v1/curated?per_page=1",
+        ),
+        headers: {"Authorization": apiKey});
+    print(response);
+  }
+
+  @override
+  void initState() {
+    categories = getCategories();
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: brandName(),
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.symmetric(horizontal: 18),
+              decoration: BoxDecoration(
+                  color: Color(0xfff5f8fd),
+                  borderRadius: BorderRadius.circular(30)),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "search Wellpaper",
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Icon(Icons.search),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Container(
+              height: 80,
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  return CategoriesTile(
+                    title: categories[index].categorieName,
+                    imgUrl: categories[index].imgUrl,
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoriesTile extends StatelessWidget {
+  final String? imgUrl, title;
+  CategoriesTile({@required this.title, @required this.imgUrl});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.only(right: 4),
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.network(
+                imgUrl.toString(),
+                height: 50,
+                width: 100,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              color: Colors.black26,
+              height: 50,
+              width: 100,
+              alignment: Alignment.center,
+              child: Text(
+                title.toString(),
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 15),
+              ),
+            )
+          ],
+        ));
+  }
+}
